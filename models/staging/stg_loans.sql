de71da2552db1730c24777d695de730b6d3ce1a4
franzzfher <78062762+franzzfher@users.noreply.github.com>
2026-02-05 06:07:23 -0600

    Update stg_loans.sql

--- models/staging/stg_loans.sql
+++ models/staging/stg_loans.sql
@@ -1,1 +1,51 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy = 'merge',
+        unique_key = '_unique_id',
+        partition_by={
+            "field": "disbursed_date",
+            "data_type": "date",
+            "granularity": "day"
+        },
+        cluster_by = ["user_id", "loan_id", "delinquency_status"],
+        require_partition_filter = false
+    )
+}}
 
+  SELECT     
+      --primary keys
+      SAFE_CAST(loan_id AS STRING) AS loan_id
+      ,SAFE_CAST(user_id AS STRING) AS user_id
+      
+      ,DATE(disbursed_date) AS disbursed_date
+      ,DATE(limit_month) AS limit_month
+  
+      --status
+      ,TRIM(delinquency_status) AS delinquency_status
+  
+      --finance
+      ,SAFE_CAST(requested_amount AS FLOAT64) AS requested_amount
+      ,SAFE_CAST(founded_amount AS FLOAT64) AS funded_amount
+      ,SAFE_CAST(capital_balance AS FLOAT64) AS capital_balance
+      ,SAFE_CAST(interest_rate AS FLOAT64) AS interest_rate
+  
+      --risk
+      ,COALESCE(SAFE_CAST(charge_off AS FLOAT64), 0) AS charge_off
+      ,COALESCE(SAFE_CAST(cogs_total_cost AS FLOAT64), 0) AS cogs_total_cost
+  
+      ,original_schedule --object
+      ,SAFE_CAST(term AS INT64) AS loan_term_months
+  
+      ,TO_HEX(MD5(CONCAT(
+          IFNULL(SAFE_CAST(loan_id AS STRING), ''), 
+          IFNULL(SAFE_CAST(user_id AS STRING), ''), 
+          IFNULL(SAFE_CAST(limit_month AS STRING), '')
+      ))) AS _unique_id
+      
+  --FROM random-480703.test.ae_challenge_loans
+  FROM {{ source('test', 'ae_challenge_loans') }}
+
+{% if is_incremental() %}
+    WHERE disbursed_date >= DATE_ADD(CURRENT_DATE(), INTERVAL -15 DAY)
+{% endif %}
