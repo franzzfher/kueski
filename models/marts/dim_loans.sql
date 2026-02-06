b22c1c717e1faaedca9196f711f6194df4f42ad6
franzzfher <78062762+franzzfher@users.noreply.github.com>
2026-02-06 04:53:45 -0600

    Create dim_loans.sql

---
+++ models/marts/dim_loans.sql
@@ -0,0 +1,77 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+SELECT
+    -- primary key
+    loan_id
+
+    -- foreign key
+    ,user_id
+
+    -- temporal
+    ,disbursed_date
+    ,vintage_month
+    ,acquisition_date
+
+    -- loan characteristics
+    ,funded_amount
+    ,requested_amount
+    ,interest_rate
+    ,loan_term_months
+    ,num_installments
+
+    -- expected amounts
+    ,expected_principal
+    ,expected_interest
+    ,expected_total_payment
+
+    -- customer info
+    ,city
+    ,state
+    ,acquisition_channel
+    ,risk_band
+    ,risk_segment
+    ,cac_amount
+
+    -- actual collections
+    ,paid_principal
+    ,paid_interest
+    ,paid_fees
+    ,paid_penalties
+    ,paid_taxes
+    ,total_collected
+    ,count_repayments
+    ,last_payment_date
+
+    -- revenue
+    ,total_revenue
+
+    -- collection rates
+    ,principal_collection_rate
+    ,interest_collection_rate
+
+    -- yield rates
+    ,expected_yield_rate
+    ,actual_yield_rate
+
+    -- costs
+    ,total_cogs
+    ,total_charge_off
+
+    -- margins
+    ,financial_margin
+    ,contribution_margin
+    ,net_margin
+
+    -- status
+    ,last_delinquency_status
+    ,final_status
+    ,dpd_bucket
+    ,is_npl
+    ,is_charged_off
+    ,last_positive_capital_balance
+
+FROM {{ ref('int_loans_enriched') }}
